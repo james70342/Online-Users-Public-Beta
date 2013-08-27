490ecece957c1dd426d349f3dacf6624ab7f7e5b
james70342 <jlusty@live.co.uk>
2013-08-27 20:05:51 +0100

    config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+	DATABASE CONFIGURATION
+**/
+$host = "localhost"; //MySQL Host e.g. localhost
+$username = "username"; //MySQL Username e.g. root
+$password = "password"; //MySQL Password e.g. 123abc
+$database = "hotel"; //MySQL Database e.g. phoenix
+/**
+	TEXT CONFIGURATION
+**/
+$bannerURL = "mosBFTLyDt20.gif"; //URL To hotel banner, must exist on same server
+$font = "EasyRider"; //Font to use. 1 - 5.
+$top = 50; //Position of text from top // you can change this!
+$left = 0; //Position of text from left  // you can change this!
+$text_before = ""; //Text before online user count // no text input
+$text_after = ""; //Text after online user count // no text input
+$text_color = "FE2E2E"; //Text color. Accepts Hex color.  // you can change this! http://www.colorpicker.com/
+$text_size = 10; //Text size  // you can change this!
+$text_rotation = 0; //Rotate that text  // you can change this!
+?>
